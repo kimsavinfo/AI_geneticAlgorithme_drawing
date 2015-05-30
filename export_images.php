9083106bcaa262b2d46ba8dc6e55b8fb49e5049c
Kim SAVAROCHE <kimsavinfo@kimsavinfo.home>
2015-05-30 20:03:31 +0200

    cleanup

--- export_images.php
+++ export_images.php
@@ -1,14 +1,23 @@
 <?php
 
 require_once("libs/file_manager.php");
+require_once('genetic/Population.php');
 
 ini_set('max_execution_time', 0);
 
-//include('img_map/france.php');
+$file_test = "uploaded/france.png";
 //include('img_map/mario_pixelise.php');
 //include('img_map/joconde.php');
 //include('img_map/landscape.php');
 //include('img_map/mario.php');
+
+
+$before = memory_get_usage();
+$population = new Population($file_test);
+$after = memory_get_usage();
+echo ($after - $before)." octets";
+
+// ancienne version : 113720 octets
 
 
 lib_exportPNGPixelsToTXT();
