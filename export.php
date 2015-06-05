ab40fda3e187aed8361c751dc19c1218f4bca40a
Kim SAVAROCHE <kimsavinfo@gmail.com>
2015-06-05 23:00:07 +0200

    export img result and send email

--- export.php
+++ export.php
@@ -18,47 +18,10 @@ initPopulation($pixels_genetic, $palette, $nb_pixels);
 $stats = array();
 evolve($pixels_genetic, $pixels_goal, $stats, $palette);
 
+// Export picture
+$export_png_path = pixelArrayToPicture($file_test, $pixels_genetic);
 
-
-
-function PixelArrayToPicture($p_width, $p_height, $p_pixels_tab)
-{
-
-	$new_pic = imagecreatetruecolor($p_width, $p_height);
-
-	$nb_of_pixels = $p_width*$p_height;
-	$current_pixel = 0;
-	$new_image_j=0;
-
-	for ($i=0; $i < $nb_of_pixels; $i+=4) { 
-		$new_color = imagecolorallocatealpha($new_pic, 
-			$p_pixels_tab[$i][0], 
-			$p_pixels_tab[$i][1],
-			$p_pixels_tab[$i][2], 
-			$p_pixels_tab[$i][3]);
-
-		$new_image_i = $current_pixel%$p_width;
-		$new_image_j = $new_image_i==0 && $i!=0 ? ($new_image_j+1) : $new_image_j;
-		$current_pixel++;
-		
-		imagesetpixel($new_pic, $new_image_i, $new_image_j, $new_color);
-	}
-
-	return $new_pic;
-}
-
-$size = getimagesize($file_test);
-header('Content-Type: image/png');
-imagepng(PixelArrayToPicture($size[0], $size[1], $pixels_genetic));
-
-
-
-
-
-
-
-
-// Sending email
+// Send email
 $to      = 'arthurlambert23@gmail.com, kimsavinfo@gmail.com';
 $subject = 'IA - Genetic Algorithme drawing';
 $headers = 'From: kimsavinfo@gmail.com' . "\r\n" ;
@@ -66,18 +29,13 @@ $headers .= 'Reply-To: kimsavinfo@gmail.com' . "\r\n" ;
 $headers .= "Content-Type: text/html; charset=\"utf-8\"";
 $headers .= 'X-Mailer: PHP/' . phpversion();
 	
-$message = $file_test .' : <br/>';
+$message = $file_test .' -> '.$export_png_path.' : <br/>';
 $message .= 'MIN_FITTING_POURCENTAGE :'.var_export($GLOBALS['MIN_FITTING_POURCENTAGE'], true).'<br/>';
 $message .=  'CROSSOVER_ACTIVATE_THRESHOLD :'.var_export($GLOBALS['CROSSOVER_ACTIVATE_THRESHOLD'], true).'<br/>';
 $message .=  'MUTATION_ACTIVATE_THRESHOLD :'.var_export($GLOBALS['MUTATION_ACTIVATE_THRESHOLD'], true).'<br/>';
 $message .=  'MAX_ITERATIONS :'.var_export($GLOBALS['MAX_ITERATIONS'], true).'<br/>';
 $message .=  'STATS :'.var_export($stats, true).'<br/>';
 
-// mail($to, $subject, $message, $headers);
-
-
-
-
-
+mail($to, $subject, $message, $headers);
 
 ?>
--- genetic/image_manager.php
+++ genetic/image_manager.php
@@ -58,3 +58,36 @@ function importPixelsRGBA($p_path, &$p_pixels, &$p_palette)
 		eval('$p_palette = '.$palette_content.';');
 	}
 }
+
+function pixelArrayToPicture($p_path, $p_pixels_tab)
+{
+	$size = getimagesize($p_path);
+	$new_pic = imagecreatetruecolor($size[0], $size[1]);
+
+	$nb_of_pixels = $size[0]*$size[1];
+	$current_pixel = 0;
+	$new_image_j=0;
+
+	for ($i=0; $i < $nb_of_pixels; $i++)
+	{ 
+		$new_color = imagecolorallocatealpha($new_pic, 
+			$p_pixels_tab[$i][0], 
+			$p_pixels_tab[$i][1],
+			$p_pixels_tab[$i][2], 
+			$p_pixels_tab[$i][3]);
+			
+		$new_image_i = $current_pixel% $size[0];
+		$new_image_j = $new_image_i==0 && $i!=0 ? ($new_image_j+1) : $new_image_j;
+		$current_pixel++;
+		
+		imagesetpixel($new_pic, $new_image_i, $new_image_j, $new_color);
+	}
+
+	$basename = basename($p_path, ".png");
+	$export_png = $GLOBALS['DIR_RESULTS'].$basename."_pixels_".microtime(true).".png";
+	
+	header('Content-Type: image/png');
+	imagepng($new_pic, $export_png);
+	
+	return $export_png;
+}
